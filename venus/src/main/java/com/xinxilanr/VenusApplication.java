c7161a268acbc3a57e41e82f911c4160d82da771
Norris Zhang <norris@intela.ai>
2017-03-11 21:40:46 +1300

    [3] flyway migrate

---
+++ venus/src/main/java/com/xinxilanr/VenusApplication.java
@@ -0,0 +1,12 @@
+package com.xinxilanr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenusApplication.class, args);
+	}
+}
