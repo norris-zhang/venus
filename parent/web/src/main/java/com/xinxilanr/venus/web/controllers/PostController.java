6f70af424c55bed403d76995d3c6779ccf8415ff
Norris Zhang <norris@intela.ai>
2017-04-17 19:06:07 +1200

    [26] publish page prototype, both [26] and [27]

--- parent/web/src/main/java/com/xinxilanr/venus/web/controllers/PostController.java
+++ parent/web/src/main/java/com/xinxilanr/venus/web/controllers/PostController.java
@@ -5,6 +5,7 @@ package com.xinxilanr.venus.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author norris
@@ -15,5 +16,9 @@ public class PostController extends BaseController {
 	@GetMapping("/posts")
 	public String listPosts() {
 		return "post/postlist";
+	}
+	@GetMapping("/publish")
+	public ModelAndView publish() {
+		return new ModelAndView("post/publish");
 	}
 }
